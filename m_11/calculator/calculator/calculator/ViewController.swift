b1236a99bb28c78a5a957b66b24ffeac8d28eaaa
shadon <in-con23@yandex.ru>
2024-03-25 12:15:05 +0500

    funcButton

--- m_11/calculator/calculator/calculator/ViewController.swift
+++ m_11/calculator/calculator/calculator/ViewController.swift
@@ -8,7 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBAction func onOne(_ sender: Any) {
+    }
+    
+    @IBAction func onTwo(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onThree(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onFour(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onFive(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onSix(_ sender: Any) {
+    }
+    
 
+    
+    
+    @IBAction func onPlus(_ sender: Any) {
+        // +
+    }
+    @IBAction func onEqual(_ sender: Any) {
+        // =
+    }
+    @IBAction func onReset(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var resulitLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

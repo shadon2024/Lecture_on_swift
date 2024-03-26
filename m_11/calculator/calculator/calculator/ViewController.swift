d2e65a14e4dd0c46df98bf395a7edabbcc85e1cc
shadon <in-con23@yandex.ru>
2024-03-26 11:47:14 +0500

    button

--- m_11/calculator/calculator/calculator/ViewController.swift
+++ m_11/calculator/calculator/calculator/ViewController.swift
@@ -8,40 +8,112 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isNewValue = true
+    var operation: MathOperation? = nil
+    // сохранит предедущую операцию
+    var previousOperation: MathOperation? = nil
+    var result: Int = 0
+    var newValue: Int = 0
     
-    
+
     @IBAction func onOne(_ sender: Any) {
+        addDigit("1")
     }
     
     @IBAction func onTwo(_ sender: Any) {
+        addDigit("2")
     }
-    
     
     @IBAction func onThree(_ sender: Any) {
+        addDigit("3")
     }
-    
     
     @IBAction func onFour(_ sender: Any) {
+        addDigit("4")
     }
-    
     
     @IBAction func onFive(_ sender: Any) {
+        addDigit("5")
     }
-    
     
     @IBAction func onSix(_ sender: Any) {
+        addDigit("6")
     }
     
-
+    @IBAction func onSeven(_ sender: Any) {
+        addDigit("7")
+    }
+    
+    @IBAction func onEight(_ sender: Any) {
+        addDigit("8")
+    }
+    
+    @IBAction func onNine(_ sender: Any) {
+        addDigit("9")
+    }
+    
+    @IBAction func onZero(_ sender: Any) {
+        addDigit("0")
+    }
+    
+    
+    
+//    @IBAction func onEqual(_ sender: Any) {
+//        // =
+//        calculate()
+//    }
+    
+    
+    @IBAction func onEqual(_ sender: Any) {
+        // =
+        calculate()
+    }
+    
     
     
     @IBAction func onPlus(_ sender: Any) {
         // +
+        operation = .sum
+        previousOperation = nil
+        isNewValue = true
+        result = getInteger()
     }
-    @IBAction func onEqual(_ sender: Any) {
-        // =
+    
+    @IBAction func onSubtract(_ sender: Any) {
+        // -
     }
+    
+    @IBAction func onMultiply(_ sender: Any) {
+        // *
+    }
+    
+    @IBAction func onDivide(_ sender: Any) {
+        // 4/2 = 2
+    }
+    
+    @IBAction func onPercent(_ sender: Any) {
+        // %
+    }
+    
+    
+    @IBAction func onComma(_ sender: Any) {
+        // ,
+    }
+    
+    
     @IBAction func onReset(_ sender: Any) {
+       isNewValue = true
+        result = 0
+        newValue = 0
+        operation = nil
+        previousOperation = nil
+        resulitLabel.text = "0"
+        
+    }
+    
+    
+    @IBAction func onClear(_ sender: Any) {
+        // clear
     }
     
     
@@ -50,9 +122,58 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resulitLabel.text = ConstansString.CalculatorTitle
+    }
+    
+    func addDigit(_ digit: String) {
+        if isNewValue {
+            resulitLabel.text = ""
+            isNewValue = false
+        }
+        var digits = resulitLabel.text
+        digits?.append(digit)
+        resulitLabel.text = digits
+    }
+    
+    func getInteger() -> Int {
+        return Int(resulitLabel.text ?? "0") ?? 0
     }
 
+    // метод каторый считает result
+    func calculate() {
+        guard let operation = operation else {
+            return
+        }
+        
+        if previousOperation != operation {
+            newValue = getInteger()
+        }
+        
+//        if operation == "+" {
+//            result += newValue
+//        } else {
+//            return
+//        }
+        result = operation.makeOperation(result: result, newValue: newValue)
+        
+        
+        previousOperation = operation
+        resulitLabel.text = "\(result)"
+        isNewValue = true
+    }
 
 }
 
+enum MathOperation {
+    case sum, substract
+    
+    func makeOperation(result: Int, newValue: Int) -> Int {
+        switch self {
+        case .sum:
+            return (result + newValue)
+        case .substract:
+            return (result - newValue)
+        }
+    }
+    
+}

ce53674931b1de1aad16a274ecb3791ff5c551fe
shadon <in-con23@yandex.ru>
2024-04-16 17:12:43 +0500

    snap kit

---
+++ m_15/m_15/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  m_15
+//
+//  Created by Admin on 15/04/24.
+//
+
+import UIKit
+import SnapKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        view.snp.contentHuggingHorizontalPriority = 0.1
+        
+        let test = HTTPMethod(rawValue: "")
+    }
+
+
+}
+
